b40a27d119df29c0a091b4e8d37b232704d4822c
Emma Liu <emmatliu@gmail.com>
2022-05-07 13:31:02 -0700

    Table returns correctly on basic menu page
    Can maybe write version for detailed menu pages also

--- src/menu_data.js
+++ src/menu_data.js
@@ -17,70 +17,111 @@ if(now.getHours() < 11 && now.getHours() > 6) {
     period = periods[3];
 }
 
-async function request(period, restaurant) {
+async function request() {
     const request = await axios.get('http://menu.dining.ucla.edu/Menus');
     return JSON.stringify(request.data);
 }
 
-function createTable(data) {
+function createTable(data,period,restaurant) {
+
+    var table = [];
+
+    var p_pattern = null;
+
+    switch(period) {
+        case 'Breakfast':
+            p_pattern = /<h2 id=\\"page-header\\">Breakfast Menu(.*?)<h2/;
+            break;
+        case 'Lunch':
+            p_pattern = /<h2 id=\\"page-header\\">Lunch Menu(.*?)<h2/;
+            break;
+        case 'Dinner':
+            p_pattern = /<h2 id=\\"page-header\\">Dinner Menu(.*?)<hr class/;
+            break;
+    }
+
+    if(p_pattern==null) {
+        return 'inv_p';
+    }
+
+    const reg_period = p_pattern;
+
+    let period_data = data.match(reg_period)[1]; // pd is now specifically the period you are in
+
+    var r_pattern = null;
+
+    switch(restaurant) {
+        case 'Epicuria':
+            r_pattern = /<h3 class=\\"col-header\\">Epicuria(.*?)<h3/;
+            break;
+        case 'De Neve':
+            r_pattern = /<h3 class=\\"col-header\\">De Neve(.*?)<h3/;
+            break;
+        case 'Bruin Plate':
+            r_pattern = /<h3 class=\\"col-header\\">Bruin Plate(.*?)/;
+            break;
+    }
+
+    if(r_pattern==null) {
+        return 'inv_r';
+    }
+
+    const reg_rest = r_pattern;
+
+    let restaurant_data = period_data.match(reg_rest)[1];
     
-    const r1 = /<li class=\\"sect-item\\">([A-za-z\s]*)</g; // Get section names
-    const r2 = /\s*([A-Za-z]*\s*[A-Za-z]*)\s*/; // Process section names
+    const r_sectGet = /<li class=\\"sect-item\\">([A-za-z\s]*)</g; // Get section names
 
-    const r3 = /<ul class=\\"item-list\\">[\\r\\n\s]*<li class=\\"menu-item\\">(.*?)<\/ul>/g; // Get section items
-    const r4 = /<a class=\\"recipelink\\" href=\\".*?\\">(.*?)<\/a>/g; // Get names in items
+    const r_itemGet = /<ul class=\\"item-list\\">[\\r\\n\s]*<li class=\\"menu-item\\">(.*?)<\/ul>/g; // Get section items
+    const r_itemName = /<a class=\\"recipelink\\" href=\\".*?\\">(.*?)<\/a>/g; // Get names in items
 
-    const r5 = /<div class=\\"item-description-wrapper\\">(.*?)<\/div>/g; // Get descriptions in items part 1
-    const r6 = /<div class=\\"tt-description\\">(.*?)<\/div>/; // Get descriptions in items part 2
+    const r_itemDe = /<div class=\\"item-description-wrapper\\">(.*?)<\/div>/g; // Get descriptions in items part 1
+    const r_itemText = /<div class=\\"tt-description\\">(.*?)<\/div>/; // Get descriptions in items part 2
 
-    let sec_names = [...data.matchAll(r1)];
-    let psn = [];
+    let sec_names = [...restaurant_data.matchAll(r_sectGet)];
+    let processed_sec_names = [];
 
-    let sec_cont = [...data.matchAll(r3)];
-    let psc = [];
+    let sec_cont = [...restaurant_data.matchAll(r_itemGet)];
+    let processed_sec_cont = [];
 
     for(let i = 0; i < sec_names.length; i++) {
         let s = sec_names[i][1].replace(/\\r\\n/g,'');
-        psn.push(s.trim());
+        processed_sec_names.push(s.trim());
     }
 
-    var k = 0;
+    let sect_lens = [];
 
     for(let i = 0; i < sec_cont.length; i++) {
-        let a = [];
-        let item_names = [...sec_cont[i][1].matchAll(r4)];
-        let item_descripts = [...sec_cont[i][1].matchAll(r5)];
+
+        let item_names = [...sec_cont[i][1].matchAll(r_itemName)];
+        sect_lens.push([processed_sec_names[i],item_names.length]);
+        
+        let item_descripts = [...sec_cont[i][1].matchAll(r_itemDe)];
+
         for(let j = 0; j < item_names.length; j++) {
+
+            let a = [];
             a[0] = item_names[j][1];
-            if(item_descripts[j][0].match(r6)!=null) {
-                let d = item_descripts[j][0].match(r6)[1].replace(/\\r\\n/g,'');
+
+            if(item_descripts[j][0].match(r_itemText)!=null) {
+                let d = item_descripts[j][0].match(r_itemText)[1].replace(/\\r\\n/g,'');
                 a[1] = d.trim();
             }
             else {
                 a[1] = "No description.";
             }
+
+            processed_sec_cont.push(a);
         }
-        psc.push(a);
     }
 
-    return psc;
+    table.push(sect_lens);
+    table.push(processed_sec_cont);
+
+    return table;
 }
 
-request("Dinner",rest).then(re => {
-    let z = createTable(re);
+request().then(re => {
+    let z = createTable(re,period,rest);
+    console.log(z);
 });
-
-
-
-/*
-split into periods -> restaurants -> sections -> individual menu items
-
-depending on page:
-find the restaurant; remove all else
-check time against meal period; find the meal period in variable; remove all else
-pattern for section names -> <li class="sect-item">([A-za-z\s]*)<
-pattern for individual items -> <a class="recipelink" href="[A-za-z:\/.0-9]*">([A-za-z\s]*)<
-
-returns table -> display that
-then display with css or whatever... idk
-*/
